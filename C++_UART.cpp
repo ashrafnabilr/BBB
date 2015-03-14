ff350df6d00ef178f378d08b12332705dad6a509
Ashraf_Nabil <ashraf.nabil.r@gmail.com>
2015-03-14 21:32:26 +0200

    first commit

---
+++ C++_UART.cpp
@@ -0,0 +1,26 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+using std::ofstream;
+
+int main()
+{
+    float x;
+    //cout << "Establishing Connection" << std::endl;
+    fstream serial ("/dev/ttyACM0");
+    //ofstream file ("Ash.txt",ios::out);
+    //std::cout << "Sending integer" << std::endl;
+    //file << 5 << std::endl; // endl does flush, which may be important
+    //std::cout << "Data Sent" << std::endl;
+    //cout << "Connection Done" << std::endl;
+   while (1){
+
+      double response;
+      serial >> response;
+    //file << x << endl ;
+    //cout << x << endl ;
+        }
+    return 0;
+}
+
+
